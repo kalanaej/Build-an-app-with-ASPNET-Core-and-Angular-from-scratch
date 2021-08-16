d55a7f4c8227d929c17cf3569841f811a72225e3
Kalana <kalanaejayasuriya@gmail.com>
2021-08-16 14:36:31 +0530

    Section 2 - Creating our first Entity

---
+++ DatingApp/API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        /* 
+            - To create a property, use 'prop' 
+        */
+
+        // Create ID property
+        public int Id { get; set; }
+
+        // Create User Name property
+        public string UserName { get; set; }
+
+        
+    }
+}
