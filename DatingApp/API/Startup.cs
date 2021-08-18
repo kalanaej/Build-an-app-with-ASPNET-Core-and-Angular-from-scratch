adb28cc9a2660b3e8d6fdb1f963cb15725e92762
Kalana <kalanaejayasuriya@gmail.com>
2021-08-18 11:58:26 +0530

    Section 3 - Displaying the fetched users in the browser

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -42,6 +42,9 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            //Add searvice for Cors
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +60,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
