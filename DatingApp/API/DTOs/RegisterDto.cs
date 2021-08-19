5496c528f0222bfa2609db91bbb45bc972c6ad67
Kalana <kalanaejayasuriya@gmail.com>
2021-08-19 16:41:20 +0530

    Section 4 - Adding validation

--- DatingApp/API/DTOs/RegisterDto.cs
+++ DatingApp/API/DTOs/RegisterDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        // [Required] means property no longer accept null values.
+
+        [Required] 
         public string Username { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
