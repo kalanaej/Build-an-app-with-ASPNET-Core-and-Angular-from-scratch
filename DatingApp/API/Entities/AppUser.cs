93fab49b1c41248e0ac74a73ca6ac380ce94213b
Kalana <kalanaejayasuriya@gmail.com>
2021-08-19 10:59:06 +0530

    Section 4 - Updating the user entity

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -12,6 +12,8 @@ namespace API.Entities
         // Create User Name property
         public string UserName { get; set; }
 
-        
+        public byte[] PasswordHash { get; set; }  
+
+        public byte[] PasswordSalt { get; set; }      
     }
 }
