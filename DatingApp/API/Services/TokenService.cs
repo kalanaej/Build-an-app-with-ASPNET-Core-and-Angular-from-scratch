91ee61248c6addc00dc8049c36a3fea2851d98d6
Kalana <kalanaejayasuriya@gmail.com>
2021-08-19 18:15:17 +0530

    Section 4 - Adding the create token logic

--- DatingApp/API/Services/TokenService.cs
+++ DatingApp/API/Services/TokenService.cs
@@ -1,14 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        // Get access to symmetric security key
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         // Genarated by selecting "ITokenService" get quick fixes and select "Implement interface".
         public string CreateToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+            // Adding our claims
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            // Creating some credentials
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Describe our token going to look like
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            // Token handler. this code line is a must to implement.
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Create the token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Return the written token
+            return tokenHandler.WriteToken(token); 
         }
     }
 }
