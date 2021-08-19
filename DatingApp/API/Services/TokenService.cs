20ec1fe830abe43f2e420283a3e9b206ad76f82e
Kalana <kalanaejayasuriya@gmail.com>
2021-08-19 17:52:47 +0530

    Section 4 - Adding a token service

---
+++ DatingApp/API/Services/TokenService.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        // Genarated by selecting "ITokenService" get quick fixes and select "Implement interface".
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
