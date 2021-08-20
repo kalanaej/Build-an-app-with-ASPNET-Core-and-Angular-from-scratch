52b61f1530cae86b9ac499f46a6db0427947d985
Kalana <kalanaejayasuriya@gmail.com>
2021-08-20 11:40:30 +0530

    Section 5 - Creating a nav bar

--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 // Manually adding http client module
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
