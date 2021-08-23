a391a637fc7f3ade759f8c16903589a21ace14a0
Kalana <kalanaejayasuriya@gmail.com>
2021-08-23 10:20:13 +0530

    Section 5 - Injecting services into components

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
 
   model: any = {}
+  loggedIn: boolean | undefined;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response =>{
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    })
   }
 
 }
