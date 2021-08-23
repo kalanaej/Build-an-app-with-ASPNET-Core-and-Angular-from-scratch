4ee22910afbd2684be2f389db02fc615afac0a4d
Kalana <kalanaejayasuriya@gmail.com>
2021-08-23 13:52:07 +0530

    Section 5 - Persisting the login

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,17 @@ export class AppComponent implements OnInit{
   users: any;
 
   // make http request to an API asyncronizly.  
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit()
   {
     this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user') ?? '{}');
+    this.accountService.setCurrentUser(user);
   }
 
   getUsers()
