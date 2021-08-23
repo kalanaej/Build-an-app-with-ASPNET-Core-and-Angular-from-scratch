809ec3423c7f665dc2684127c2c736961ee9081f
Kalana <kalanaejayasuriya@gmail.com>
2021-08-23 14:34:14 +0530

    Section 5 - Using the async pipe (part 1)

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
+
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -10,18 +13,17 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {}
-  loggedIn: boolean | undefined;
+  currentUser$: Observable<User> | undefined;
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response =>{
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     })
@@ -29,15 +31,5 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user =>{
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    })
-  }
-
 }
