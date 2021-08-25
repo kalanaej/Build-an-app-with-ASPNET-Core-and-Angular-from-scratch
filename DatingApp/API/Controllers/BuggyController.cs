5f8e33e0192c7b33b325ad254d20227f7cf2994e
Kalana <kalanaejayasuriya@gmail.com>
2021-08-25 10:39:53 +0530

    Section 7 - Handling server errors (part 1)

--- DatingApp/API/Controllers/BuggyController.cs
+++ DatingApp/API/Controllers/BuggyController.cs
@@ -1,3 +1,4 @@
+using System;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -39,11 +40,21 @@ namespace API.Controllers
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
-            var thing = _context.Users.Find(-1);
 
-            var thingToReturn = thing.ToString();
+            try
+            {
+                var thing = _context.Users.Find(-1);
 
-            return thingToReturn;
+                var thingToReturn = thing.ToString();
+
+                return thingToReturn;
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Computer says no!");
+            }
+
+            
         }
 
 
