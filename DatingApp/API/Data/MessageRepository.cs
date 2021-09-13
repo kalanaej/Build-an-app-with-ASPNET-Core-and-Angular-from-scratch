07f5107ae25dbe4c070f74a2a9188f1384423781
Kalana <kalanaejayasuriya@gmail.com>
2021-09-13 14:11:07 +0530

    Section 15 - Deleting messages on the client

--- DatingApp/API/Data/MessageRepository.cs
+++ DatingApp/API/Data/MessageRepository.cs
@@ -34,10 +34,7 @@ namespace API.Data
 
         public async Task<Message> GetMessage(int id)
         {
-            return await _context.Messages
-                .Include(u => u.Sender)
-                .Include(u => u.Recipient)
-                .SingleOrDefaultAsync(x => x.Id == id);
+            return await _context.Messages.Include(u => u.Sender).Include(u => u.Recipient).SingleOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
