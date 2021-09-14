0cb544c6a7fd1b3c78d4e5f4de0ad90e01f0d723
Kalana <kalanaejayasuriya@gmail.com>
2021-09-14 10:04:34 +0530

    Section 16 - Configuring the DbContext

--- DatingApp/API/Data/DataContext.cs
+++ DatingApp/API/Data/DataContext.cs
@@ -1,17 +1,18 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole, 
+                               IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         // Double click on DbContext and click quick fixes and select constructor(options)
         // remove [NotNullAttribute] part
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-
-        public DbSet<AppUser> Users { get; set; }
 
         public DbSet<UserLike> Likes { get; set; }
 
@@ -21,6 +22,19 @@ namespace API.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+
+            builder.Entity<AppRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+
 
             builder.Entity<UserLike>().HasKey(k => new {k.SourceUserId, k.LikedUserId});
 
